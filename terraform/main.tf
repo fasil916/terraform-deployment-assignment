a59b114642fd2782f5160d16b208142b4695e870
fasil916 <71936145+fasil916@users.noreply.github.com>
2025-11-05 17:47:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,3 +19,25 @@ output "aks_cluster_id" {
 output "aks_resource_group_id" {
   value = module.aks_cluster.resource_group_id
 }
+
+
+
+
+
+resource "azurerm_storage_account" "tf_backend_storage" {
+  name                     = "tfstatestore12345"
+  resource_group_name      = azurerm_resource_group.tf_backend_rg.name
+  location                 = azurerm_resource_group.tf_backend_rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  allow_blob_public_access = false
+}
+
+# Create storage container for Terraform state
+resource "azurerm_storage_container" "tf_backend_container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tf_backend_storage.name
+  container_access_type = "private"
+}
+
+
