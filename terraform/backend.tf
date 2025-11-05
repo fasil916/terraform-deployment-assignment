6179cdacec8e54838cb3e9e2cb533b085ece5e64
fasil916 <71936145+fasil916@users.noreply.github.com>
2025-11-05 18:14:43 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstatestore12345"
+    storage_account_name = "tfstatestorewebapp88"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
